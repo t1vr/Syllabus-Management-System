e3b96db1e2814121c43f7de338809daa81eddc24
t1vr <tanviramd32@gmail.com>
2023-07-13 15:56:11 +0600

    db contexts configured

---
+++ Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,65 @@
+﻿using Domain;
+using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Infrastructure.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Persistence.Context;
+
+//public class ApplicationDbContext : BaseDbContext
+//{
+//    public ApplicationDbContext(ITenantInfo currentTenant, DbContextOptions options)
+//        : base(currentTenant, options)
+//    {
+//    }
+
+//    public DbSet<Program> Programs => Set<Program>();
+//    //public DbSet<Course> Courses => Set<Course>();
+//    //public DbSet<Syllabus> Syllabuses => Set<Syllabus>();
+//    //public DbSet<AppUser> AppUserProfiles => Set<AppUser>();
+//    //public DbSet<CourseUser> CourseUsers => Set<CourseUser>();
+
+
+
+//    protected override void OnModelCreating(ModelBuilder modelBuilder)
+//    {
+//        modelBuilder.Entity<Program>().IsMultiTenant();
+//        //modelBuilder.Entity<Course>().IsMultiTenant();
+//        //modelBuilder.Entity<Syllabus>().IsMultiTenant();
+//        //modelBuilder.Entity<AppUser>().IsMultiTenant();
+//        //modelBuilder.Entity<CourseUser>().HasKey(x => new { x.CourseId, x.AppUserId });
+
+//        base.OnModelCreating(modelBuilder);
+//        modelBuilder.HasDefaultSchema(SchemaNames.Portal);
+//    }
+//}
+
+public class ApplicationDbContext : BaseDbContext
+{
+    public ApplicationDbContext(ITenantInfo currentTenant, DbContextOptions options, IOptions<DatabaseSettings> dbSettings)
+        : base(currentTenant, options, dbSettings)
+    {
+    }
+
+    public DbSet<Program> Programs => Set<Program>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Syllabus> Syllabuses => Set<Syllabus>();
+    public DbSet<AppUser> AppUserProfiles => Set<AppUser>();
+    public DbSet<CourseUser> CourseUsers => Set<CourseUser>();
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Program>().IsMultiTenant();
+        modelBuilder.Entity<Course>().IsMultiTenant();
+        modelBuilder.Entity<Syllabus>().IsMultiTenant();
+        modelBuilder.Entity<AppUser>().IsMultiTenant();
+        modelBuilder.Entity<CourseUser>().IsMultiTenant();
+
+        modelBuilder.Entity<CourseUser>().Ignore(x => x.Id).HasKey(x => new { x.CourseId, x.AppUserId });
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema(SchemaNames.Portal);
+    }
+}
--- SMS-API/Program.cs
+++ SMS-API/Program.cs
@@ -1,3 +1,13 @@
+using Application.Multitenancy;
+using Infrastructure.Identity;
+using Infrastructure.Multitenancy;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Context;
+using Infrastructure.Persistence.Initialization;
+using Infrastructure.Persistence.Seeder;
+using Microsoft.EntityFrameworkCore;
+using Shared.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +17,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<TenantDbContext>(m => m.UseNpgsql(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"), 
+    b => b.MigrationsAssembly(typeof(TenantDbContext).Assembly.FullName)))
+            .AddMultiTenant<SMPTenantInfo>()
+                .WithClaimStrategy(FSHClaims.Tenant)
+                .WithEFCoreStore<TenantDbContext, SMPTenantInfo>()
+                .Services
+            .AddScoped<ITenantService, TenantService>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(m => m.UseNpgsql(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"), 
+    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+builder.Services.AddIdentity();
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)))
+    .AddDbContext<ApplicationDbContext>(m => m.UseNpgsql(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"),
+        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)))
+    .AddTransient<IDatabaseInitializer, DatabaseInitializer>()
+    .AddTransient<ApplicationDbInitializer>()
+    .AddTransient<ApplicationDbSeeder>();
+
 var app = builder.Build();
+await app.Services.CreateScope().ServiceProvider.GetRequiredService<IDatabaseInitializer>().InitializeDatabasesAsync(default!);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,7 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
+app.UseMultiTenant();
 
 app.UseAuthorization();
 
